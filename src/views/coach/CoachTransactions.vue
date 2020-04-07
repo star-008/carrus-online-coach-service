e97e277dd4b9922cbdf47934a3114b3a769a7ce8
star008 <star008@protonmail.com>
2020-04-07 10:48:52 +0300

    Add coach transactions screen

---
+++ src/views/coach/CoachTransactions.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <h2 class="mb-3">Orders</h2>
+    <Order
+      v-for="order in this.$store.state.orders"
+      :key="order.id"
+      :order="order"
+    />
+  </v-container>
+</template>
+
+<script>
+import Order from '@/components/common/Order'
+
+export default {
+  components: {
+    Order,
+  },
+}
+</script>
