31ee21595c6014e856fb39f6b385e7f1ef98771f
star008 <star008@protonmail.com>
2020-04-07 10:44:32 +0300

    Add coach profile setting screen

---
+++ src/views/coach/CoachMyProfile.vue
@@ -0,0 +1,19 @@
+<template>
+  <CoachProfile :coach-id="user.uid" :can-edit="true" />
+</template>
+
+<script>
+import CoachProfile from '@/components/common/CoachProfile'
+
+export default {
+  components: {
+    CoachProfile,
+  },
+
+  computed: {
+    user() {
+      return this.$store.state.auth.user
+    },
+  },
+}
+</script>
